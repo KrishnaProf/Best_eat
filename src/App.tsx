0bbd8a4f534f7d0d0017e307e9fd4f1c0f3e0b46
Leela_Krishna <leela_krishna@dell.com>
2024-06-24 13:46:00 +0530

    code updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Foods from "./components/Foods";
+import Category from "./components/Category";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <HeadlineCards />
+      <Foods />
+      <Category />
     </>
   );
 }
