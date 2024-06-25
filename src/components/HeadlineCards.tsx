f23dee10a4a74a99b27475b2cc96a6f01861ed41
Krishna <leelakrishna682@gmail.com>
2024-06-25 13:04:49 +0530

    Update HeadlineCards.tsx

--- src/components/HeadlineCards.tsx
+++ src/components/HeadlineCards.tsx
@@ -4,7 +4,7 @@ const HeadlineCards = () => {
   return (
     <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6 ">
       <div className="rounded-xl relative">
-        {/* Overlay */}
+        {/* Overlay  */}
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
           <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
           <p className="px-2">Through 8/26</p>
